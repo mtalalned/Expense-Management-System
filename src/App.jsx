d1669aa0230d6b3b243ea0a368a395a2b22f77ea
mtalalned <muhammadtalalned@gmail.com>
2024-12-07 11:04:32 +0500

    UI in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useState } from 'react'
 import { useForm } from "react-hook-form";
+import './App.css'
 
 const App = () => {
   
-  const { register, handleSubmit, watch, formState: { errors } , setValue , trigger} = useForm();
+  const { register, handleSubmit, formState: { errors } , setValue , trigger} = useForm();
   const cashArray = ['Cashin' , 'Cashout']
   const cashoutArray = ['groceries' , 'fuel' , 'food/drink' , 'car/bike' , 'taxi' , 'clothes' , 'shopping' , 'entertainment' , 'electricity']
   const cashinArray = ['salary' , 'business' , 'investment' , 'loan']
@@ -103,37 +104,37 @@ const App = () => {
 
 
   return (
-    <>
-    <div className='flex justify-center gap-[150px]'>
-      <div className='flex flex-col justify-center gap-4'>
-        <p>Cash in</p>
-        <p>{cashInTotalArray.reduce((acc , CVal) => {
-          return acc + +CVal.inputValue 
-        } , 0)}</p>
+    <div className='flex flex-col justify-center items-center gap-5 py-7'>
+    
+      <div className='flex justify-center gap-[150px] border border-black p-5 w-[80%]'>
+        <div className='flex flex-col justify-center gap-4'>
+          <p>Cash in</p>
+          <p>{cashInTotalArray.reduce((acc , CVal) => {
+            return acc + +CVal.inputValue 
+          } , 0)}</p>
+        </div>
+        <div className='flex flex-col justify-center gap-4'>
+          <p>Cash out</p>
+          <p>{cashoutTotalArray.reduce((acc , CVal) => {
+            return acc + +CVal.inputValue 
+          } , 0)}</p>
+        </div>
+        <div className='flex flex-col justify-center gap-4'>
+          <p>Balance</p>
+          <p>{cashInTotalArray.reduce((acc , CVal) => {
+            return acc + +CVal.inputValue 
+          } , 0) - cashoutTotalArray.reduce((acc , CVal) => {
+            return acc + +CVal.inputValue 
+          } , 0)}</p>
+        </div>
       </div>
-      <div className='flex flex-col justify-center gap-4'>
-        <p>Cash out</p>
-        <p>{cashoutTotalArray.reduce((acc , CVal) => {
-          return acc + +CVal.inputValue 
-        } , 0)}</p>
-      </div>
-      <div className='flex flex-col justify-center gap-4'>
-        <p>Balance</p>
-        <p>{cashInTotalArray.reduce((acc , CVal) => {
-          return acc + +CVal.inputValue 
-        } , 0) - cashoutTotalArray.reduce((acc , CVal) => {
-          return acc + +CVal.inputValue 
-        } , 0)}</p>
-      </div>
-    </div>
 
 
 
 
-    <div>
 
 
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex justify-center items-center gap-5 border border-black w-[80%]'>
         
           <div>
             <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs" {...register('inputValue' , {required: true})}/>
@@ -206,40 +207,39 @@ const App = () => {
           <button type='submit' className="btn btn-active btn-primary">Done</button>
         
       </form>
-    </div>
 
 
 
 
 
 
-    <div className='flex flex-col justify-center items-center gap-2'>
+    <div className='flex flex-col justify-start items-center gap-2 w-[80%] h-[380px] border border-black py-5 bg-slate-50 rounded-md relative'>
       
-      <div className='flex justify-center gap-5'>
-        <button className="btn btn-active btn-primary w-[75px]">S.No</button>
-        <button className="btn btn-active btn-primary w-[175px]">Nature</button>
-        <button className="btn btn-active btn-primary w-[175px]">Category</button>
-        <button className="btn btn-active btn-primary w-[175px]">Source</button>
-        <button className="btn btn-active btn-primary w-[175px]">Amount {'Rs.'}</button>
-        <button className="btn w-[100px]">Action</button>
+      <div className='flex justify-center gap-5 w-full fixed'>
+        <button className="w-[75px] py-2 rounded-md bg-amber-400 font-semibold text-white hover:bg-amber-500 transition duration-300 ease-in-out">S.No</button>
+        <button className="w-[175px] py-2 rounded-md bg-amber-400 font-semibold text-white hover:bg-amber-500 transition duration-300 ease-in-out">Nature</button>
+        <button className="w-[175px] py-2 rounded-md bg-amber-400 font-semibold text-white hover:bg-amber-500 transition duration-300 ease-in-out">Category</button>
+        <button className="w-[175px] py-2 rounded-md bg-amber-400 font-semibold text-white hover:bg-amber-500 transition duration-300 ease-in-out">Source</button>
+        <button className="w-[175px] py-2 rounded-md bg-amber-400 font-semibold text-white hover:bg-amber-500 transition duration-300 ease-in-out">Amount {'Rs.'}</button>
+        <button className="bg-green-400 py-2 hover:bg-green-500 rounded-md text-white w-[100px] font-semibold">Action</button>
       </div>
 
+      <div className='absolute top-[70px] h-[275px] scrollbar-custom'>
       {expenseArray.map ((item , index)=> {
-        return <div className='flex justify-center items-center gap-5 border-b border-b-2 py-3'>
+        return <div className='flex justify-center items-center gap-5 border-b border-b-2 py-3 w-full'>
         <p className='w-[75px] text-center'>{index+1}</p>
         <p className='w-[175px] text-center'>{item.NatureofCash}</p>
         <p className='w-[175px] text-center'>{item.CashinValue}{item.CashOutValue}</p>
         <p className='w-[175px] text-center'>{item.Sourceoffunds || 'Nil'}</p>
         <p className='w-[175px] text-center'>{item.inputValue}</p>
-        <button className="bg-error rounded text-white py-1 w-[100px]" onClick={()=> deleteItem (index)}>Delete</button>
+        <button className="bg-error rounded-md text-white py-1 w-[100px] hover:bg-red-500 transition duration-300 ease-in-out font-semibold" onClick={()=> deleteItem (index)}>Delete</button>
       </div>
       })}
+      </div>
       
-      
-
     </div>
     
-    </>
+    </div>
   )
 }
 
